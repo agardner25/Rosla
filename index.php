81dc3d213ea4100ce8fdae306d2d44a937eed8df
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-03-05 14:09:54 +0000

    Add CSS outline and title

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rosla</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-    
+<div class="container"><h1>Welcome to Rosla</h1></div>
 </body>
 </html>
